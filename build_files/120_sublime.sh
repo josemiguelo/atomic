cc2d11a4f0a61bcfbaad6e192bc4d252fde52323
Jose Miguel Ochoa <josemiguelo.ochoa@gmail.com>
2026-03-23 19:20:21 -0500

    fix architecture on sublime script

--- build_files/120_sublime.sh
+++ build_files/120_sublime.sh
@@ -5,7 +5,7 @@ set -ouex pipefail
 echo "::group:: ===$(basename "$0")==="
 
 rpm -v --import https://download.sublimetext.com/sublimehq-rpm-pub.gpg
-dnf5 config-manager addrepo --from-repofile=https://download.sublimetext.com/rpm/stable/x86_64/sublime-text.repo
+dnf5 config-manager addrepo --from-repofile=https://download.sublimetext.com/rpm/stable/$(uname -m)/sublime-text.repo
 dnf5 makecache --repo=sublime-text
 dnf5 install -y sublime-text
 
